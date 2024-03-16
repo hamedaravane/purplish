add75b5d17086ed74ed8879dd428b7a18326b9fd
hamed <h.arghavan@ompfinex.com>
2024-03-16 20:27:58 +0330

    create custom-websocket.ts

--- src/app/prices/infrastructure/kucoin.infra.ts
+++ src/app/prices/infrastructure/kucoin.infra.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { InfraAbstract } from '@shared/abstract/infra.abstract';
 import {KucoinCurrencyListData, KucoinPublicBulletResponse} from '@prices/data-access/entity/kucoin.entity';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { CustomWebsocket } from '@shared/abstract/custom-websocket';
 
 @Injectable({
@@ -9,9 +9,16 @@ import { CustomWebsocket } from '@shared/abstract/custom-websocket';
 })
 export class KucoinInfra extends InfraAbstract {
   protected readonly kucoinSpotBaseUrl: string = "https://api.kucoin.com";
+  protected readonly kucoinWebsocketSpotBaseUrl: string = "wss://ws-api-spot.kucoin.com/?token=";
   public readonly kucoinIconPath: string = "https://assets.staticimg.com/cms/media/3gfl2DgVUqjJ8FnkC7QxhvPmXmPgpt42FrAqklVMr.png";
-  private readonly customWebsocket = new CustomWebsocket();
+  private customWebsocket!: CustomWebsocket;
   public getKucoinPublicTokenWebsocket(): Observable<KucoinPublicBulletResponse> {
-    return this.httpClient.post<KucoinPublicBulletResponse>(`${this.kucoinSpotBaseUrl}/api/${this.versionsEnum.V1}/bullet-public`, null);
+    return this.httpClient.post<KucoinPublicBulletResponse>(`${this.kucoinSpotBaseUrl}/api/${this.versionsEnum.V1}/bullet-public`, null)
+      .pipe(
+        map((response) => {
+          this.customWebsocket = new CustomWebsocket(this.kucoinWebsocketSpotBaseUrl + response.data.token);
+          return response;
+        })
+      );
   }
 }
