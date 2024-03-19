bb6c072214e895765c0e82fb19bb489e97b90acb
Hamed Arghavan <hamedaravane@gmail.com>
2024-03-19 18:03:27 +0330

    update facades

--- src/app/market/data-access/kucoin.facade.ts
+++ src/app/market/data-access/kucoin.facade.ts
@@ -1,49 +1,22 @@
 import {inject, Injectable} from "@angular/core";
-import {Observable, Subject} from "rxjs";
-import {OmpfinexFacade} from '@market/data-access/ompfinex.facade';
-import {KucoinWebsocketMarketSnapshotData, KucoinWebsocketMessage} from '@market/entity/kucoin.entity';
-import {KucoinWebsocket} from "@market/infrastructure/websocket/kucoin.websocket";
+import {KucoinWebsocket} from '@market/infrastructure/websocket/kucoin.websocket';
+import {MarketData} from '@market/entity/kucoin.entity';
+import {map} from 'rxjs';
 
 @Injectable({
   providedIn: "root"
 })
 export class KucoinFacade {
   private readonly kucoinWebSocket = inject(KucoinWebsocket);
-  private readonly ompfinexFacade = inject(OmpfinexFacade);
-  private readonly ompfinexCurrenciesMap = this.ompfinexFacade.ompfinexCurrenciesMapGetter;
-  private kucoinCurrencyMap = new Map<string, KucoinWebsocketMarketSnapshotData>();
-  private kucoinMarketDataSubject = new Subject<Map<string, KucoinWebsocketMarketSnapshotData>>();
+  private kucoinCurrencyMap = new Map<string, MarketData>();
   readonly kucoinIconPath = this.kucoinWebSocket.kucoinIconPath;
-  public get kucoinMarketData$(): Observable<Map<string, KucoinWebsocketMarketSnapshotData>> {
-    return this.kucoinMarketDataSubject.asObservable();
-  }
-  protected onMessageReceived(msg: KucoinWebsocketMessage): void {
-    switch (msg.type) {
-      case "welcome":
-        this.sendMessage(
-          {
-            id: msg.id,
-            type: 'subscribe',
-            topic: '/market/snapshot:USDS',
-            response: true
-          }
-        )
-        setInterval(() => {
-          this.sendMessage({id: msg.id, type: 'ping'})
-        }, this.instanceServer.pingTimeout - 100);
-        break;
-      case "pong":
-        break;
-      case "ack":
-        break;
-      case "message":
-        if (this.ompfinexCurrenciesMap.has(msg.data!.data.baseCurrency)) {
-          this.kucoinCurrencyMap.set(msg.data!.data.baseCurrency, msg.data!);
-          this.kucoinMarketDataSubject.next(this.kucoinCurrencyMap)
-        }
-        break;
-    }
-  }
+
+  kucoinMarketDataMap = this.kucoinWebSocket.messages$.pipe(
+    map(value => {
+      this.kucoinCurrencyMap.set(value.baseCurrency, value);
+      return this.kucoinCurrencyMap;
+    })
+  );
 
   initWebSocket() {
     this.kucoinWebSocket.init().then();
