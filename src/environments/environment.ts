a2e964d68d816103e77b56c3f1bde1de2a1037cb
Hamed Arghavan <hamedaravane@gmail.com>
2024-03-22 21:40:53 +0330

    create environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,13 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  version: '0.0.0',
+  platform: 'web',
+  bundleVersionCode: 110,
+  applicationName: 'Purplish',
+  ompfinexApiBaseUrl: 'https://api.ompfinex.com',
+  ompfinexStreamBaseUrl: 'wss://stream.ompfinex.com/stream',
+  ompfinexOrigin: 'ompfinex.com',
+  kucoinApiBaseUrl: 'https://api.kucoin.com',
+  kucoinStreamBaseUrl: 'wss://ws-api-spot.kucoin.com/?token=',
+  kucoinOrigin: 'kucoin.com'
+};
