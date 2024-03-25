1ea6dcd61eda18b2e973125df55dbc155bdd5ae9
Amirhossein Majidi <a.majidy@ompfinex.com>
2024-03-25 17:24:29 +0330

    get binance data

--- src/app/market/infrastructure/websocket/binance.websocket.ts
+++ src/app/market/infrastructure/websocket/binance.websocket.ts
@@ -3,12 +3,14 @@ import {MarketStore} from "@market/store/market.store";
 import {inject, Injectable} from "@angular/core";
 import {filter, map} from "rxjs";
 import {environment} from "@environment";
+import {BinanceSocket, BinanceStreamData, convertToBinanceDto} from "@market/entity/binance.entity";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BinanceWebsocket extends WebsocketAbstract{
   private _marketStore = inject(MarketStore)
+  private _binanceMarketMap = new Map<string, BinanceStreamData>()
   protected endpoint!: string;
 
 
@@ -28,12 +30,13 @@ export class BinanceWebsocket extends WebsocketAbstract{
             this.endpoint = this.endpoint + '/' + market + '@trade'
           }
         })
-        console.log(this.endpoint)
         this.connect()
       })
   }
-  protected handleMessages(message: any): void {
-    console.log(message)
+  protected handleMessages(message: BinanceSocket): void {
+    const binanceMarketData = convertToBinanceDto(message)
+    this._binanceMarketMap.set(binanceMarketData.symbol.slice(0,binanceMarketData.symbol.indexOf('USDT')), binanceMarketData)
+    this._marketStore.binanceMarketDtoMapSubject.next(this._binanceMarketMap)
   }
 
   protected onComplete(): void {
